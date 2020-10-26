aba27913152dbd21bb8faac9239b6a3dea478f94
Mattia Bruno <38449948+mbruno46@users.noreply.github.com>
2020-10-27 00:33:45 +0100

    custom decoder initialized inside bison; single numpy.int supported

--- bison/io.py
+++ bison/io.py
@@ -47,6 +47,9 @@ def save(fname, *args):
     class Encoder(json.JSONEncoder):
         
         def _encode(self, data):
+            if hasattr(data,'__dict__'):
+                tag = f'__{data.__class__.__module__}.{data.__class__.__name__}__'
+                return {tag: self._encode(data.__dict__)}
 
             if isinstance(data,tuple):
                 return {'__tuple__': True, 'data': [self._encode(e) for e in data]}
@@ -54,6 +57,8 @@ def save(fname, *args):
                 return {'__list__': True, 'data': [self._encode(e) for e in data]}                
             elif isinstance(data,dict):
                 return {key: self._encode(value) for key, value in data.items()}
+            elif isinstance(data,range):
+                return {'__range__': True, 'data': [data.start, data.stop, data.step]}
             elif isinstance(data,numpy.ndarray):
                 if little:
                     crc32 = get_crc32(data.tobytes("C"))
@@ -62,9 +67,11 @@ def save(fname, *args):
                     crc32 = get_crc32(data.byteswap().tobytes("C"))
                 book[id(data)] = data
                 return {'__numpy.ndarray__': True, 'crc32': crc32, 'shape': numpy.shape(data), 'dtype': data.dtype.str}
-            elif isinstance(data,complex):
+            elif isinstance(data,(complex)):
                 return {'__complex__': True, 'data': [self._encode(data.real), self._encode(data.imag)]}
             else:
+                if isinstance(data,(numpy.int32,numpy.int64)):
+                    return int(data)
                 return data
 
         def default(self, obj):
@@ -91,7 +98,7 @@ def save(fname, *args):
                 f.write(array.tobytes("C"))
             else:
                 f.write(array.byteswap().tobytes("C"))
-                
+            
     dt += time.time()
     print(f'[Bison]: Written {n/1024.**2:g} MB at {n/1024.**2/dt:g} MB/s')
     
@@ -105,8 +112,10 @@ def load(fname, decoder=None):
     
     if not decoder is None:
         if not type(decoder) is list:
-            decoder = [decoder]
-    
+            decs = [decoder()]
+        else:
+            decs = [d() for d in decoder]
+            
     with open(fname, 'rb') as f:
         n = int.from_bytes(f.read(4), 'little')
         header = f.read(n).decode('utf-8')
@@ -114,7 +123,7 @@ def load(fname, decoder=None):
         
         def decode_hook(obj):
             if not decoder is None:
-                for dec in decoder:
+                for dec in decs:
                     n = f'__{dec.type}__'
                     if n in obj:
                         return dec.decode(obj[n])
@@ -123,6 +132,8 @@ def load(fname, decoder=None):
                 return tuple(obj['data'])
             elif '__list__' in obj:
                 return list(obj['data'])
+            elif '__range__' in obj:
+                return range(*obj['data'])
             elif '__numpy.ndarray__' in obj:
                 dt = numpy.dtype(obj['dtype'])
                 sh = tuple(obj['shape'])
@@ -139,7 +150,7 @@ def load(fname, decoder=None):
                 return complex(*obj['data'])
             else:
                 return obj
-            
+        
         res = json.loads(header, object_hook=decode_hook)
       
     dt += time.time()
