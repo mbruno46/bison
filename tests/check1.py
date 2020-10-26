3d341ec48f17b7b0b4dc520ad76675b044426a02
Mattia Bruno <38449948+mbruno46@users.noreply.github.com>
2020-10-27 00:36:54 +0100

    added single numpy.int to check

--- tests/check1.py
+++ tests/check1.py
@@ -7,8 +7,9 @@ atuple = (4,5,(6,))
 array = numpy.arange(32**4).astype('i4')
 astr = 'hello there'
 afl = 3.14
+numpys = [numpy.int32(32), numpy.int64(64)]
 
-bison.save('test1',alist, atuple, atuple, array, astr, afl)
+bison.save('test1',alist, atuple, atuple, array, astr, afl, numpys)
 
 res = bison.load('test1')
 
@@ -18,6 +19,7 @@ assert (atuple == res[2])
 assert numpy.all(array == res[3])
 assert (astr == res[4])
 assert (afl == res[5])
+assert numpy.all(numpys == res[6])
 
 try:
     bison.save('test1')
